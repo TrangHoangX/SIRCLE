39c782f205eba6711420469b8ce8170097c22ea9
Trang Hoang <38724201+TrangHoangX@users.noreply.github.com>
2023-07-13 10:59:54 +0200

    Add files via upload

---
+++ js/_vti_cnf/jquery.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|16 Sep 2012 13:58:28 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|eng_contact.html eng_info.html eng_intranet.html eng_index.html de_projekt.html de_intranet.html slider.dwt eng_consortium.html slider_eng.dwt de_info.html index.html de_konsortium.html de_kontakt.html eng_project.html
---
+++ js/_vti_cnf/slider.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|16 Sep 2012 13:58:33 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|eng_contact.html eng_info.html eng_intranet.html eng_index.html de_projekt.html de_intranet.html slider.dwt eng_consortium.html slider_eng.dwt de_info.html index.html de_konsortium.html de_kontakt.html eng_project.html
+vti_author:SR|powl-THINK\\powl
+vti_modifiedby:SR|powl-THINK\\powl
+vti_timecreated:TR|16 Sep 2012 13:58:33 -0000
+vti_cacheddtm:TX|16 Sep 2012 13:58:33 -0000
+vti_filesize:IR|12898
